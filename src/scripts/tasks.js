876141b57db90b222b16457d4b18c8e8ec8d7f67
wdfwwkkee <denis.usmanov101@mail.ru>
2023-11-27 09:55:24 +0500

    asd

--- src/scripts/tasks.js
+++ src/scripts/tasks.js
@@ -3,6 +3,7 @@
 
 
 // Основные переменные
+next = document.getElementById('next');
 counter = document.querySelector('.counter');
 correct = document.querySelector('.correct');
 firstNum = document.querySelector('.first-num');
@@ -10,11 +11,27 @@ secondaryNum = document.querySelector('.secondary-num');
 answerBtn = document.querySelectorAll(".var-btn");
 
 //Функция рандома
-function rand(){
+function rand() {
     x = Math.ceil(Math.random(10) * 10)
     return(x)
 }
 
+function nextLevel() {
+    st = rand();
+    nd = rand();
+    nextbtnanswer = Math.ceil((Math.ceil(Math.random(0.5) * 10)) / 2.5);            
+    firstNum.innerText = st;
+    document.getElementById("1st").innerHTML = st;
+    secondaryNum.innerText = nd;
+    document.getElementById("2nd").innerHTML = nd;
+    answer = parseInt(firstNum.innerText) + parseInt(secondaryNum.innerText) 
+    document.getElementById("1v").innerHTML = document.getElementById("1v").innerText = rand();
+    document.getElementById("2v").innerHTML = document.getElementById("2v").innerText = rand();
+    document.getElementById("3v").innerHTML = document.getElementById("3v").innerText = rand();
+    document.getElementById("4v").innerHTML = document.getElementById("4v").innerText = rand();
+    document.getElementById(nextbtnanswer + "v").innerHTML = document.getElementById(nextbtnanswer + "v").innerText = answer;
+    answer = 0;
+}
 //Сохранение правильных ответов
 cookieStorage = window.localStorage
 
@@ -36,24 +53,18 @@ answerBtn.forEach((el)=> {
             correct.innerHTML = 'Правильно';
             correct.style.color = "Green";
             counter.innerHTML = counter.innerText = parseInt(document.getElementById("counter").innerText) + 1;
-            st = rand();
-            nd = rand();
-            nextbtnanswer = Math.ceil((Math.ceil(Math.random(0.5) * 10)) / 2.5);            
-            firstNum.innerText = st;
-            document.getElementById("1st").innerHTML = st;
-            secondaryNum.innerText = nd;
-            document.getElementById("2nd").innerHTML = nd;
-            answer = parseInt(firstNum.innerText) + parseInt(secondaryNum.innerText) 
-            document.getElementById("1v").innerHTML = document.getElementById("1v").innerText = rand();
-            document.getElementById("2v").innerHTML = document.getElementById("2v").innerText = rand();
-            document.getElementById("3v").innerHTML = document.getElementById("3v").innerText = rand();
-            document.getElementById("4v").innerHTML = document.getElementById("4v").innerText = rand();
-            document.getElementById(nextbtnanswer + "v").innerHTML = document.getElementById(nextbtnanswer + "v").innerText = answer;
-            answer = 0;
+            next.style.display = 'inline';
+            document.getElementById("answer-box").innerHTML = document.getElementById("answer-box").innerText = parseInt(el.innerText);
         }
         else {
             correct.style.color = "Red";
             correct.innerHTML = 'Неправильно';
         }
     })
+})
+
+next.addEventListener('click', () => {
+    next.style.display = 'none';
+    nextLevel();
+    document.getElementById("answer-box").innerHTML = document.getElementById("answer-box").innerText = ""  ;
 })
